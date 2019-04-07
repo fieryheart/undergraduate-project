a8212c1ab5a14c8e79fb3695e220983d1ab36acc
fieryheart <fiery_heart@163.com>
2019-04-07 20:56:06 +0800

    2019-04-07-add-face-detection

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,15 @@
+            // if(faceRects.size()) {
+            //     boundingBox_temp.start_x = faceRects[0].x;
+            //     boundingBox_temp.start_y = faceRects[0].y;
+            //     boundingBox_temp.width = faceRects[0].width;
+            //     boundingBox_temp.height = faceRects[0].height;
+            //     boundingBox_temp.centroid_x = boundingBox_temp.start_x + boundingBox_temp.width / 2.0;
+            //     boundingBox_temp.centroid_y = boundingBox_temp.start_y + boundingBox_temp.height / 2.0;
+
+
+            //     // Mat_<double> current_shape = regressor.Predict(frame_flip, boundingBox_temp, initial_number);
+            //     image_temp = frame_flip.clone();
+            //     // for(int i = 0;i < landmark_num;i++){
+            //     //     circle(image_temp,Point2d(current_shape(i,0),current_shape(i,1)),3,Scalar(255,0,0),-1,8,0);
+            //     // }
+            // }
