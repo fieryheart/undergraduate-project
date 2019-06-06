8e9492638732d3300e06cd7d74c1f6c0c6f2ed40
fieryheart <fiery_heart@163.com>
2019-06-06 14:25:30 +0800

    2019-06-06-Combine-OpenCV-and-dlib

--- FaceRecognition/main.cpp
+++ FaceRecognition/main.cpp
@@ -1,17 +1,35 @@
 /*
  * @Author: fieryheart
  * @Date: 2019-06-06 10:48:12
- * @LastEditTime: 2019-06-06 12:07:00
+ * @LastEditTime: 2019-06-06 14:23:38
  * @Description: This program is based on OpenCV and dlib.
  * @Description: If you find and bugs, please email me: fiery_heart@163.com
  */
 
-#include "./src/FaceRecognition.h";
+#include "./src/FaceRecognition.h"
+
+using namespace cv;
+using namespace dlib;
 
 int main()
 {
     FaceRecognition project;
+    image_window win;
+    
     project.SetCameraCapture(0, 480, 320);
-    project.LaunchCamera();
+
+    // Test: Starting Camera
+    // project.LaunchCamera();
+    
+    while(1) {
+        Mat frame = project.GetCamreaFrame();
+        matrix<bgr_pixel> image;
+        image = project.MatToMatrix(frame);
+        // image = project.DetectFace(image);
+        win.clear_overlay();
+        win.set_image(image);
+        if(waitKey(30) >= 0) break;
+    }
+     
     return 0;
 }
--- FaceRecognition/src/FaceRecognition.h
+++ FaceRecognition/src/FaceRecognition.h
@@ -1,7 +1,7 @@
 /*
  * @Author: fieryheart
  * @Date: 2019-06-06 10:50:53
- * @LastEditTime: 2019-06-06 12:08:12
+ * @LastEditTime: 2019-06-06 14:23:44
  * @Description: This program is based on OpenCV and dlib.
  * @Description: If you find and bugs, please email me: fiery_heart@163.com
  */
@@ -12,6 +12,10 @@
 #include <iostream>
 #include <string>
 #include <opencv2/opencv.hpp>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/gui_widgets.h>
+#include <dlib/image_io.h>
+#include <dlib/opencv.h>
 
 class FaceRecognition {
     public:
@@ -32,6 +36,11 @@ class FaceRecognition {
             capture.set(CV_CAP_PROP_FRAME_WIDTH, width);
 	        capture.set(CV_CAP_PROP_FRAME_HEIGHT, height);
         }
+        dlib::matrix<dlib::bgr_pixel> MatToMatrix(cv::Mat frame) {
+            dlib::matrix<dlib::bgr_pixel> img;
+            dlib::assign_image(img, dlib::cv_image<dlib::bgr_pixel>(frame));
+            return img;
+        }
         void LaunchCamera() {
             if(!capture.isOpened()) {
                 std::cout << "The camera is not open." << std::endl;
@@ -44,6 +53,12 @@ class FaceRecognition {
                 cv::imshow("Frame: ", dst_frame);
                 if(cv::waitKey(30) >= 0) break;
             }
+        }
+        cv::Mat GetCamreaFrame() {
+            cv::Mat src_frame, dst_frame;
+            capture >> src_frame;
+            cv::flip(src_frame, dst_frame, 1);
+            return dst_frame;
         }
         FaceRecognition() {
             
